e1b4024cfb6856f23fbc4d8821233701d5de813c
jaziel126 <chuan1131481289@gmail.com>
2020-07-18 20:23:44 +0800

    顺序栈第一次编写，完成栈的构建、初始化

--- SequentialStorageOfStacks/源.cpp
+++ SequentialStorageOfStacks/源.cpp
@@ -20,4 +20,30 @@ typedef struct {/*顺序栈的结构定义*/
 
 /*函数原型说明*/
 Status InitStack(SqStack& S);/*构建空栈*/
-Status Push(SqStack S, SElemType& e);/*元素入栈*/
+Status Push(SqStack &S);/*元素入栈*/
+Status Pop(SqStack& S);/*元素出栈*/
+Status DestroyStack(SqStack& S);/*销毁栈*/
+
+/*算法实现*/
+Status InitStack(SqStack& S) {
+	S.base = (SElemType*)malloc(STACK_INIT_SIZE * sizeof(SElemType));/*分配空间*/
+	if (!S.base) exit(OVERFLOW);
+	S.top = S.base;
+	return OK;
+}
+Status Push(SqStack& S) {
+	if (S.top - S.base >= S.stacksize) {/*栈满，重新分配空间*/
+		S.base = (SElemType*)realloc(S.base, (S.stacksize + STACKINCREMENT) * sizeof(SElemType));
+		if (!S.base) exit(OVERFLOW);
+		S.top = S.base + S.stacksize;
+		S.stacksize += STACKINCREMENT;
+	}
+	int n;
+	cout << "入栈元素个数：";
+	cin >> n;
+	for (int i = 0;i < n;i++) {
+		cin >> *S.top;
+		S.top++;
+	}
+	return OK;
+}
